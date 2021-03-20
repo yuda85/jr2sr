9069ac7194174607c362024aa1d4d82e71c35951
didimango <53489397+didimango@users.noreply.github.com>
2021-03-20 11:45:48 +0200

    fixed login page

--- src/app/components/pages/login-page/login-page.component.ts
+++ src/app/components/pages/login-page/login-page.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public emailSignIn(email: string, pasword: string) {
+    this.authService.SignIn(email, pasword);
   }
 
+  public googleSignin(): void {
+    this.authService.GoogleAuth();
+  }
 }
